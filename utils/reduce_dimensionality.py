8e970b8257a0d2e3600ef6eb187f6ae20c039f66
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-11-01 17:03:00 -0400

    Smoothening logic for LF bands in all images. Added Appconfig

--- utils/reduce_dimensionality.py
+++ utils/reduce_dimensionality.py
@@ -1,15 +1,17 @@
 import argparse
 from os import listdir
 from os.path import isfile, join
+from scipy.io import loadmat, savemat
+from configparser import ConfigParser
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import logging
-from scipy.io import loadmat, savemat
 
-from datapaths import DATASET_PATH, HYPER_BANDS_FILE, HYPER_FOLDER_PATH
+
+from datapaths import DATASET_PATH, HYPER_BANDS_FILE, HYPER_FOLDER_PATH, CONFIG_FILE
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(lineno)d:%(message)s')
@@ -23,6 +25,25 @@ def trim_bands(source_list, filter_list):
             trimmed.append(source_list[i])
     return trimmed
 
+def smoothen(y, box_pts):
+    return np.convolve(y, np.ones(box_pts)/box_pts, mode='same')
+
+def smoothen_hyper_image(image):
+    pos = int(config['RED_DIM']['SMOOTH_START_POS'])
+    conv_size = int(config['RED_DIM']['CONV_SIZE'])
+for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            image[i][j] = np.append(
+                image[i][j][:pos], smoothen(image[i][j][pos:], conv_size))
+    return image 
+
+    
+def smoothen_hyper_images():
+    #TODO Change this to work on all files 
+    for hyper_file in hyper_files:
+        smooth_image = smoothen_hyper_image(
+            loadmat(HYPER_FOLDER_PATH + hyper_file)['image'])
+        savemat(HYPER_FOLDER_PATH + hyper_file, {'image': smooth_image})
 
 def purge_noisy_bands(image, band_filter):
     # 369 usable bands per pixel
@@ -35,8 +56,11 @@ def purge_noisy_bands(image, band_filter):
 
 def get_configured_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--merge-bands', default=False, dest='merge_bands',
-                        help='Merge bands with averaging for bands with low symmetric KL Divergence')
+    #TODO Decide whether to keep or ditch this - Use matlab scripts to make this happen 
+    # parser.add_argument('--merge-bands', default=False, dest='merge_bands',
+    #                     help='Merge bands with averaging for bands with low symmetric KL Divergence')
+    parser.add_argument('--smoothen-only', default=False, dest='smoothen_only',
+                        help='Turns on only smoothing of reduced dimensionality data across bands for all images from the configured starting position')
     return parser
 
 
@@ -52,24 +76,39 @@ def fetch_file_names():
 
 
 def clean_hyper_images():
-    for hyper_file in hyper_files[3:4]:
-        clean_image = purge_noisy_bands(
-            loadmat(HYPER_FOLDER_PATH + hyper_file)['image'], hyper_bands['Noise_flag'])
+    for hyper_file in hyper_files:
+        clean_image = smoothen_hyper_image(purge_noisy_bands(
+            loadmat(HYPER_FOLDER_PATH + hyper_file)['image'], hyper_bands['Noise_flag']))
         savemat(HYPER_FOLDER_PATH + hyper_file, {'image': clean_image})
+        
+# Maximal/Minimal Noise Fraction
+# Generate documentation out of code automatically 
+# NDVI Normalized Differential Vegetation Index 
 
-
+# Standard deviation in crown areas. 
 if __name__ == '__main__':
     log.info('Starting Execution')
-    parser = get_configured_parser()
+    config = ConfigParser() 
+    config.read(CONFIG_FILE)
 
+    # TODO Decide whether this is needed after using COTS for KL Divergence based dimensionality reduction
+    parser = get_configured_parser()
     args = parser.parse_args()
-    args.merge_bands = bool(args.merge_bands)
+    # args.merge_bands = bool(args.merge_bands)
+    args.smoothen_only = bool(args.smoothen_only)
 
     hyper_files, hyper_bands = fetch_file_names()
     log.info('Fetched all the Hyperspectral File Names')
+    if args.smoothen_only and bool(config['RED_DIM']['SMOOTHEN']):
+        log.info('Just Smoothening Hyper Images')
+        smoothen_hyper_images()
+        log.info('Execution Completed')
+        exit(123)
+
+    log.info('Attempting to Clean off all the noisy bands and smoothen the curves')
     clean_hyper_images()
-    log.info('Clean off all the noisy bands')
-    if args.merge_bands:
-        # TODO Add Band Merging using KL Divergence here
-        pass
+
+    # if args.merge_bands:
+    #     # TODO Add Band Merging using KL Divergence here
+    #     pass
     log.info('Execution Completed')
