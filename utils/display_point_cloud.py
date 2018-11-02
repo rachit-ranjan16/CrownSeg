5bad4ac61cc56258bddf1ded0914441e9b4a8d62
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-11-02 19:52:19 -0400

    Bug Fixes

--- utils/display_point_cloud.py
+++ utils/display_point_cloud.py
@@ -13,7 +13,7 @@ POINT_CLOUD_FILE = dirname(dirname(abspath(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser() 
-    parser.add_argument('--pc-id', default=3,dest='pc_id', choices=[str(x) for x in range(1,52) if x not in [12,22,23,24,45,46,47,49]])
+    parser.add_argument('--pc-id', default='3',dest='pc_id', choices=[str(x) for x in range(1,52) if x not in [12,22,23,24,45,46,47,49]])
     args = parser.parse_args() 
     args.pc_id = args.pc_id if len(args.pc_id) == 2 else '0' + args.pc_id
     pc_df = pd.read_csv(POINT_CLOUD_FILE + args.pc_id + '.csv')
@@ -27,3 +27,4 @@ if __name__ == '__main__':
     surf = ax.plot_trisurf(X, Y, Z,
     linewidth=0.8, antialiased=True)
     plt.show()
+    print(X.shape, Y.shape, Z.shape)
